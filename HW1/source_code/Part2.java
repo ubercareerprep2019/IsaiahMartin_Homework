33d8ba959bd5d78fa692544e4094e277df45332d
Isaiah Martin <isaiah.t.martin.21@dartmouth.edu>
2019-06-14 17:55:21 -0400

    yes

--- HW1/source_code/Part2.java
+++ HW1/source_code/Part2.java
@@ -6,24 +6,52 @@ import java.util.*;
 
 class Part2{
     /*If s1 is the same length as s2 and every element in s1 is within s2, then it is a permutation */
-    public static boolean isStringPermutation(String s1, String s2)
-    {
-//        If length of s1 != length of s2, return false
-        if (s1.length() != s2.length()) {
+    public static boolean isStringPermutation(String s1, String s2) {
+        //check for same size
+        if(s1.length() != s2.length())
+        {
             return false;
         }
-        //otherwise make sure each element in string 1 is contained in string 2
-        else {
-            int i = 0;
-            while (i < s1.length()) {
-               if(! s2.contains(Character.toString(s1.charAt(i))))
-               {
-                    return false;
-               }
-                i++;
+        //check for same letter occurances
+        int m = 0;
+        while (m < s1.length()) {
+            if(! s2.contains(Character.toString(s1.charAt(m))))
+            {
+                return false;
             }
-            return true;
+            m++;
         }
+        //check for same letter frequencies
+        HashMap<Character, Integer> value_counts = new HashMap<Character, Integer>();
+        for (int i = 0; i < s1.length(); i++) {
+            Character chr = s1.charAt(i);
+            if (!value_counts.containsKey(chr)) {
+                value_counts.put(chr, 1);
+            } else {
+                value_counts.put(chr, value_counts.get(chr) + 1);
+            }
+        }
+
+        HashMap<Character, Integer> value_counts2 = new HashMap<Character, Integer>();
+        for (int k = 0; k < s2.length(); k++) {
+            Character chr2 = s2.charAt(k);
+            if (!value_counts2.containsKey(chr2)) {
+                value_counts2.put(chr2, 1);
+            } else {
+                value_counts2.put(chr2, value_counts2.get(chr2) + 1);
+            }
+        }
+        //if they don't have same frequency of letters return false
+        for (Character letter : value_counts.keySet())
+        {
+            //System.err.println(letter);
+            //System.err.print(value_counts.get(letter));
+            //System.err.print(value_counts2.get(letter));
+            if ((value_counts.get(letter)) != value_counts2.get(letter)) {
+                return false;
+            }
+        }
+        return true;
     }
 
     /* Array<Pair<int>> pairsThatEqualSum(Array<int> inputArray, int targetSum)
@@ -61,25 +89,31 @@ class Part2{
 
 
     public static void main(String[] args){
-        /*
+
         //Tests for isStringPermutation
         //a permutation
+        System.out.print("Expected: true; Result: ");
         System.out.println(isStringPermutation("cane","enac")); //true
         //a non-permutation
+        System.out.print("Expected: false; Result: ");
         System.out.println(isStringPermutation("nakii","akn")); //false
         //a permutation
+        System.out.print("Expected: true; Result: ");
         System.out.println(isStringPermutation("magic","camgi"));  //true
         //empty strings
+        System.out.print("Expected: true; Result: ");
         System.out.println(isStringPermutation("","")); //true
         //same strings
+        System.out.print("Expected: true; Result: ");
         System.out.println(isStringPermutation("thesaurus","thesaurus")); //true
         //a non-permutation
+        System.out.print("Expected: false; Result: ");
         System.out.println(isStringPermutation("thesaaras","thesaurus")); //false
-        */
 
-        /*
+
+
         //Tests for pairsThatEqualSum
-        //array {5,5,2,8,1}
+        System.out.println("array {5,5,2,8,1} // target 10");
         ArrayList<Integer> numbers = new ArrayList<Integer>(5);
         numbers.add(5);
         numbers.add(5);
@@ -87,7 +121,7 @@ class Part2{
         numbers.add(8);
         numbers.add(1);
         System.out.println(pairsThatEqualSum(numbers,10));
-        //array {2,2,2,8,5}
+        System.out.println("array {2,2,2,8,5} // targer 10");
         ArrayList<Integer> numbers2 = new ArrayList<Integer>(5);
         numbers2.add(2);
         numbers2.add(2);
@@ -95,7 +129,7 @@ class Part2{
         numbers2.add(8);
         numbers2.add(5);
         System.out.println(pairsThatEqualSum(numbers2,10));
-        //array {-12,2,9,20,-5,-17,0,-3}
+        System.out.println("array {-12,2,9,20,-5,-17,0,-3} // target -3");
         ArrayList<Integer> numbers3 = new ArrayList<Integer>(8);
         numbers3.add(-12);
         numbers3.add(2);
@@ -106,6 +140,6 @@ class Part2{
         numbers3.add(0);
         numbers3.add(-3);
         System.out.println(pairsThatEqualSum(numbers3,-3));
-        */
+
     }
 }
